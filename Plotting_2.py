6a53ce4037fb3a4c49465fdaad1ff2399eda75dd
FranziskaScheller <60098877+FranziskaScheller@users.noreply.github.com>
2022-10-24 12:51:16 +0200

    Change size of axis

---
+++ Plotting_2.py
@@ -0,0 +1,121 @@
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pandas as pd
+import geopandas
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from shapely.geometry import Polygon
+#import warnings
+#warnings.filterwarnings(action=”ignore”)
+
+def create_geodf_from_df(df: pd.DataFrame, lon_name: str = 'longitude', lat_name: str = 'latitude') -> geopandas.GeoDataFrame:
+    """ Create Geopandas.GeoDataFrame from df using the longitudinal and latitudinal information in columns lon_name and lat_name
+    :param df: DataFrame with geographical information in columns lon_name and lat_name
+    :param lon_name: column name with longitudinal information (default 'lon')
+    :param lat_name: column name with latitudinal information (default 'lat')
+    :return: geopandas.GeoDataFrame including columns of df
+    """
+    return geopandas.GeoDataFrame(df, geometry=geopandas.points_from_xy(x=df[lon_name], y=df[lat_name]))
+
+# def scatter_points_germany(gdf: geopandas.GeoDataFrame,
+#                            col_to_plot: str,
+#                            ax: [plt.Axes, None],
+#                            plot_colorbar: bool = True,
+#                            **kwds_plot) -> plt.Axes:
+#     """ Create scatter plot of values in gdf[col_to_plot] over boundary of germany.
+#     :param gdf: geopandas data frame
+#     :param col_to_plot: the column of gdf to plot
+#     :param ax: (optional) a plt.Axes object where to plot
+#     :param plot_colorbar: whether to plot a colorbar besides the plot
+#     :param kwds_plot: passed to geopandas scatter function
+#     :return: plt.Axes object with plot
+#     """
+#     world = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
+#     germany = world[world['name'] == 'Germany']
+#     if ax is None:
+#         fig, ax = plt.subplots()
+#     germany.boundary.plot(edgecolor='black', ax=ax)
+#     ax.set(xlabel='longitude', ylabel='latitude')
+#     if plot_colorbar:
+#         divider = make_axes_locatable(ax)
+#         cax = divider.append_axes("right", size="5%", pad=0.1)
+#         gdf.plot(ax=ax, marker='.', column=col_to_plot, alpha=.1, aspect='1.3', legend=True, cax=cax, **kwds_plot)
+#     else:
+#         gdf.plot(ax=ax, marker='.', column=col_to_plot, alpha=.1, aspect='1.3', **kwds_plot)
+#     return ax
+
+def scatter_points_europe(gdf: geopandas.GeoDataFrame,
+                           col_to_plot: str,
+                           ax: [plt.Axes, None],
+                           plot_colorbar: bool = True,
+                           **kwds_plot) -> plt.Axes:
+    """ Create scatter plot of values in gdf[col_to_plot] over boundary of germany.
+    :param gdf: geopandas data frame
+    :param col_to_plot: the column of gdf to plot
+    :param ax: (optional) a plt.Axes object where to plot
+    :param plot_colorbar: whether to plot a colorbar besides the plot
+    :param kwds_plot: passed to geopandas scatter function
+    :return: plt.Axes object with plot
+    """
+    world = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
+    europe = world[(world.continent == "Europe") & (world.name != "Russia")]
+
+    # Create a custom polygon
+    polygon = Polygon([(-22, 26.5), (45.5, 26.5), (45.5, 72.5), (-22, 72.5)])
+    # poly_gdf = geopandas.GeoDataFrame([1], geometry=[polygon], crs=world.crs)
+    # fig, ax = plt.subplots()
+    # ax = europe.plot(ax=ax)
+    # poly_gdf.plot(edgecolor=”red”, ax = ax, alpha = 0.1)
+    # plt.show()
+
+    europe = geopandas.clip(europe, polygon)
+    europe.plot()
+
+    if ax is None:
+        fig, ax = plt.subplots()
+    europe.boundary.plot(edgecolor='black', ax=ax)
+    ax.set(xlabel='longitude', ylabel='latitude')
+    #if plot_colorbar:
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.1)
+    #aspect='1.3',
+    gdf.plot(ax=ax, marker='.', column=col_to_plot, alpha=.1, legend=True, cax=cax, **kwds_plot)
+    ax.set(title='test')
+    # else:
+    #     gdf.plot(ax=ax, marker='.', column=col_to_plot, alpha=.1, aspect='1.3', **kwds_plot)
+    #gdf.plot(ax=ax, marker='.', column=col_to_plot, alpha=.1, aspect='1.3', **kwds_plot)
+
+    return ax
+
+# Load data
+
+DF_Data = pd.read_csv(
+    'MeanClimatologicalData_DF_DE_reshaped.csv', header=0, index_col=None, sep=';')
+
+gdf_loadings = create_geodf_from_df(DF_Data)
+
+#world = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
+#europe=world[world.continent==”Europe”]
+
+# for season in seasons:
+#     wind_speeds, lat, lon, time = read_nc4_file(file=nc4_file)
+#     df_mean_std = pd.DataFrame({
+#         'lat': lat,
+#         'lon': lon,
+#         'mean': np.mean(wind_speeds, axis=0),
+#         'std': np.sqrt(np.var(wind_speeds, axis=0))
+#     })
+#     gdf_mean_std = create_geodf_from_df(df_mean_std)
+gdf_loadings = create_geodf_from_df(DF_Data)
+
+fig, axes = plt.subplots(1, 2, figsize=(20, 10))
+ax = axes[0]
+scatter_points_europe(gdf_loadings, 'msl_DE', ax=ax, cmap='summer')
+#ax.set_title('Mean [$m/s$]')
+
+
+ax = axes[1]
+scatter_points_europe(gdf_loadings, 'msl_DE', ax=ax, cmap='summer')
+#ax.set_title('Standard deviation [$m/s$]')
+fig.tight_layout()
+fig.savefig('test.png')
